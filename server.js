53375c19d22ee745637111ade70f812c4a734c8e
Toteya <111172685+Toteya@users.noreply.github.com>
2024-12-10 18:33:59 +0200

    allow for handling of larger data

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ import router from './routes/index';
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+// Allow for JSON data and Handle large payloads
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.use('/', router);
+
 app.listen(port);
 
 export default app;
